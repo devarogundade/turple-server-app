35c6fcd80cf4b586348bb4bbd0e48960dd5bbce8
devarogundade <devarogundade@gmail.com>
2023-05-15 15:25:22 +0100

    update

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -4,12 +4,9 @@ const graphAPI = require("../utils/SubGraphAPI")
 const Ad = db.ads
 const App = db.apps
 
-const fee = '100000000000000000'
-
 exports.loadAds = async (req, res) => {
     try {
-        const subid = req.query.subid
-        const appId = subid.replace('subid_', '')
+        const appId = req.query.subid.replace('subid_', '')
 
         const app = await graphAPI.getApp(appId)
         const ads = await graphAPI.getAds(app.category, app.format)
@@ -23,46 +20,48 @@ exports.loadAds = async (req, res) => {
     }
 }
 
-exports.debitAd = async (req, res) => {
-    Ad.findOneAndUpdate(
-        { adId: req.query.adId }, // filter
-        { $inc: { spent: fee } }, // data
-        {
-            upsert: true,
-            returnNewDocument: true,
-            returnDocument: "after"
-        } // options
-    ).then(result => {
-        const data = { status: 'OK', data: result }
-        res.send(data)
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some err occurred."
-        })
-    })
-}
+exports.onAdWatch = async (req, res) => {
+    try {
+        const fee = req.query.fee ? req.query.fee : 0
 
-exports.creditApp = async (req, res) => {
-    App.findOneAndUpdate(
-        { adId: req.query.adId }, // filter
-        { $inc: { earned: fee } }, // data
-        {
-            upsert: true,
-            returnNewDocument: true,
-            returnDocument: "after"
-        } // options
-    ).then(result => {
-        const data = { status: 'OK', data: result }
+        const result1 = await Ad.findOneAndUpdate(
+            { adId: req.query.adid }, // filter
+            { $inc: { spent: fee } }, // data
+            {
+                upsert: true,
+                returnNewDocument: true,
+                returnDocument: "after"
+            } // options
+        )
+
+        const result2 = await App.findOneAndUpdate(
+            { adId: req.query.subid }, // filter
+            { $inc: { earned: fee } }, // data
+            {
+                upsert: true,
+                returnNewDocument: true,
+                returnDocument: "after"
+            } // options
+        )
+
+        const data = {
+            status: 'OK',
+            result1: result1,
+            result2: result2
+        }
+
         res.send(data)
-    }).catch(err => {
+    } catch (err) {
         res.status(500).send({
             message: err.message || "Some err occurred."
         })
-    })
+    }
+
+
 }
 
 exports.ad = async (req, res) => {
-    Ad.findById(req.params.id)
+    Ad.find({ adId: req.params.id })
         .then(result => {
             const data = { status: 'OK', data: result }
             res.send(data);
@@ -76,7 +75,7 @@ exports.ad = async (req, res) => {
 
 
 exports.app = async (req, res) => {
-    App.findById(req.params.id)
+    App.find({ appId: req.params.id })
         .then(result => {
             const data = { status: 'OK', data: result }
             res.send(data);
--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -2,7 +2,10 @@ module.exports = app => {
     const controller = require("../controllers/index.controller.js")
     const router = require("express").Router()
 
+    router.get("/ad/:id", controller.ad)
+    router.get("/app/:id", controller.app)
     router.get("/loadads", controller.loadAds)
+    router.post('/onadwatch', controller.onAdWatch)
 
     app.use("/", router);
 };
